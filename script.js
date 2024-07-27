bfa56f21483ba880c232b608c40ebdf06c21c6fe
azuremodded <grab.next.fact@clkdmail.com>
2024-07-27 13:12:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(() => {
-        const loadingScreen = document.getElementById('loadingScreen');
-        const container = document.getElementById('container');
-
-        loadingScreen.style.opacity = '0';
-        loadingScreen.style.visibility = 'hidden';
-
-        setTimeout(() => {
-            document.body.style.visibility = 'visible';
-            container.classList.add('fade-in');
-        }, 1000);
-    }, 1000); // 1 sec loading time
+    const referrer = document.referrer;
+    if (!referrer.includes("tinyurl.com")) {
+        document.body.innerHTML = `
+            <div class="container">
+                <h1 class='error'>Access Denied. Please use the proper redirect link.</h1>
+                <button class='link-button' onclick="window.location.href='https://tinyurl.com';">Go To TinyURL</button>
+            </div>
+        `;
+        return;
+    }
 
     const messageDiv = document.getElementById('message');
     const encryptedText = "bG9hZHN0cmluZyhnYW1lOkh0dHBHZXQoImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9BY3R5cm4vU2NyaXB0cy9tYWluL0F6dXJlTW9kZGVkIikpKCk=";
@@ -49,7 +47,7 @@ document.addEventListener("mousemove", function(event) {
     const centerX = rect.width / 2;
     const centerY = rect.height / 2;
 
-    const rotateX = ((centerY - y) / centerY) * 5; 
+    const rotateX = ((centerY - y) / centerY) * 5;
     const rotateY = ((x - centerX) / centerX) * 5;
 
     image.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg) scale(1.3)`;
