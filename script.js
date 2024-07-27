5294ba842ed6a08f7d2d906e2424f2ee6415835f
azuremodded <grab.next.fact@clkdmail.com>
2024-07-27 12:05:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        const loadingScreen = document.getElementById('loadingScreen');
+        loadingScreen.classList.add('hidden');
+    }, 2000);
+
     const messageDiv = document.getElementById('message');
     const encryptedText = "bG9hZHN0cmluZyhnYW1lOkh0dHBHZXQoImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9BY3R5cm4vU2NyaXB0cy9tYWluL0F6dXJlTW9kZGVkIikpKCk=";
     const decodedText = atob(encryptedText);
 
-    messageDiv.innerHTML = `<p>Azure Modded - DaHood Script</p>`;
+    messageDiv.innerHTML = `
+<p>Azure Modded - DaHood Script</p>`;
     document.getElementById('copyArea').value = decodedText;
     document.querySelector('.copy-text').style.display = 'block';
     document.querySelector('.copy-button').style.display = 'block';
@@ -37,7 +43,7 @@ document.addEventListener("mousemove", function(event) {
     const centerY = rect.height / 2;
 
     const rotateX = ((centerY - y) / centerY) * 5;
-    const rotateY = ((x - centerX) / centerX) * 5;
+    const rotateY = ((x - centerX) / centerX) * 5; 
 
     image.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg) scale(1.3)`;
 });
